0492c230c944b0149d22a4eeacc674f6079f09e9
svenradimiri <svenradimiri@gmail.com>
2019-10-02 21:28:19 +0200

    initial commit

---
+++ moodchecker.py
@@ -0,0 +1,12 @@
+mood = input("How do You feel? ")
+
+if mood == "happy":
+    print("Nice to hear that!")
+elif mood == "nervous":
+    print("Take a deep brath!")
+elif mood == "sad":
+    print("It's gonna be better!")
+elif mood == "relaxed":
+    print("Cool!")
+else:
+    print("Mood not recognized!")
